6bf88e682874297ec0e085f317b38a7e424bc64f
yaraikun <evan.calupig@gmail.com>
2025-07-19 17:23:19 +0800

    style: adjusted a few lines to adhere to c89
    I don't know why I still use c89 standards.

--- src/sort.c
+++ src/sort.c
@@ -4,6 +4,7 @@
 void SwapStr(String str1, String str2)
 {
     String temp;
+
     strcpy(temp, str1);
     strcpy(str1, str2);
     strcpy(str2, temp);
@@ -13,7 +14,8 @@ void SortStrArr(String strArr[], int size)
 {
     int i, j, min_idx;
 
-    if (size <= 1) return;
+    if (size <= 1)
+        return;
 
     for (i = 0; i < size - 1; i++) {
         min_idx = i;
