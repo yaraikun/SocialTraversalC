589ed08758c653bfe6ccbf58440c4c20719dd51f
yaraikun <evan.calupig@gmail.com>
2025-07-19 17:30:02 +0800

    feat: added dfs/bfs function prototypes

--- include/file_io.h
+++ include/file_io.h
@@ -9,5 +9,7 @@ void WriteSet(const char *filename, GraphType graph);
 void WriteDegree(const char *filename, GraphType graph);
 void WriteList(const char *filename, GraphType graph);
 void WriteMatrix(const char *filename, GraphType graph);
+void WriteBFS(const char *filename, GraphType graph, int startVertexIndex);
+void WriteDFS(const char *filename, GraphType graph, int startVertexIndex);
 
 #endif // FILE_IO_H
