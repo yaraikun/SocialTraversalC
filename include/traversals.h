68c4b0c3f2a6bb09d76ea57094cd02abf72a94dd
yaraikun <evan.calupig@gmail.com>
2025-07-19 16:52:53 +0800

    feat: created traversals prototypes

---
+++ include/traversals.h
@@ -0,0 +1,9 @@
+#ifndef TRAVERSALS_H
+#define TRAVERSALS_H
+
+#include "graph.h"
+
+void BFS(GraphType graph, int startVertexIndex, String result[]);
+void DFS(GraphType graph, int startVertexIndex, String result[]);
+
+#endif // TRAVERSALS_H
